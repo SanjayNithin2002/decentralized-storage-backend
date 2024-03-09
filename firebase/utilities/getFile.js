3676e63621fc84469e9636bdc023763544d1bada
SanjayNithin2002 <sanjayfornewletters@gmail.com>
2024-03-10 00:26:32 +0530

    better firebase organization

---
+++ firebase/utilities/getFile.js
@@ -0,0 +1,28 @@
+const fetchPDF = require('../../api/middlewares/utilities/fetchPDF');
+
+const getFile = (bucket, filepath) => {
+    return new Promise((resolve, reject) => {
+        const file = bucket.file(filepath);
+        var options = {
+            version: 'v4',
+            action: 'read',
+            expires: Date.now() + 1000 * 60 * 60 * 24 * 7 //one week
+        };
+        file.getSignedUrl(options)
+            .then(url => {
+                const [folderName, fileName] = filepath.split('/');
+                const downloadFilePath = `downloads/${fileName}`;
+                fetchPDF(url, downloadFilePath)
+                    .then(results => resolve(results))
+                    .catch(err => reject(err));
+            }
+            )
+            .catch(err => {
+                reject({
+                    error: err,
+                    status: err.status
+                })
+            }
+            );
+    })
+}
