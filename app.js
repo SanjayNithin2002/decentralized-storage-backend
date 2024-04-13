c9f35f67c5c5208f90fe8b538f8698869c5cc462
SanjayNithin2002 <sanjayfornewletters@gmail.com>
2024-04-13 19:54:05 +0530

    handling cors

--- app.js
+++ app.js
@@ -20,6 +20,29 @@ app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    const allowedOrigins = ['http://localhost:3000', 'postman://app'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin) || origin === undefined) {
+        res.header("Access-Control-Allow-Origin", origin || "*");
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Accept, Authorization, Content-Type"
+        );
+        if (req.method === 'OPTIONS') {
+            res.header(
+                "Access-Control-Allow-Methods",
+                "PUT, POST, PATCH, DELETE, GET"
+            );
+            return res.status(200).json({});
+        }
+        next();
+    } else {
+        return res.status(403).json({ message: "Forbidden" });
+    }
+});
+
+
 // Routes
 app.get('/', (req, res) => {
     res.status(200).json({
