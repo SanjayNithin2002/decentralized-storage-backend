7eaab819fcd80cb6f56b720a008d970259c647ed
SanjayNithin2002 <sanjayfornewletters@gmail.com>
2024-04-15 16:17:36 +0530

    added deployment link cors allowed origins

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    const allowedOrigins = ['http://localhost:3000', 'postman://app'];
+    const allowedOrigins = ['http://localhost:3000', 'https://decentralized-storage-frontend.onrender.com', 'postman://app'];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin) || origin === undefined) {
         res.header("Access-Control-Allow-Origin", origin || "*");
